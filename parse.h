e633d1022d01da8144c546c3e6a3e9029d40856f
Ville Helin <ville.helin@gmail.com>
2021-03-11 22:33:09 +0200

    Removed unused functions

--- parse.h
+++ parse.h
@@ -2,9 +2,6 @@
 #ifndef _PARSE_H
 #define _PARSE_H
 
-int input_number(void);
-int input_next_string(void);
-int get_next_plain_string(void);
 int get_next_token(void);
 int compare_next_token(char *token);
 int expand_macro_arguments(char *in);
